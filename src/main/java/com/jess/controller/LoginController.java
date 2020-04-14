ce510195d40e9dbdb7ae88973b3695735e50eea3
jessGra <jesus.egranada@gmail.com>
2020-04-14 15:13:15 -0500

    Complete

---
+++ src/main/java/com/jess/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.jess.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping({"/","/login"})
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/menu")
+	public String menu() {
+		return "menu";
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+
+}
